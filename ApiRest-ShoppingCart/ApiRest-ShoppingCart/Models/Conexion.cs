35d3cacfe0c9b6e116efd8570d746a8339c240af
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2020-11-22 21:09:05 -0600

    Se agregó la clase para la conexion a la base de datos[

---
+++ ApiRest-ShoppingCart/ApiRest-ShoppingCart/Models/Conexion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ApiRest_ShoppingCart.Models
+{
+    public class Conexion
+    {
+        private static string user = "sa";
+        private static string password = "admin";
+        private static string server = "GODLIKE";
+        private static string database = "ShoppingCart";
+
+        public static string getConString()
+        {
+            return "Persist Security Info = false; User ID = '" + user
+                      + "'; Password = '" + password
+                      + "'; Initial Catalog = '" + database
+                      + "'; Server = '" + server + "'";
+        }
+
+        public static SqlCommand commandSP(string SP)
+        {
+            string strConnection = Conexion.getConString();
+            SqlConnection myConString = new SqlConnection(strConnection);
+            SqlCommand command = new SqlCommand(SP, myConString);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            return command;
+        }
+
+        public static DataTable execCommandSelect(SqlCommand command)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                command.Connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = command;
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                command.Connection.Dispose();
+                command.Connection.Close();
+            }
+            return dt;
+        }
+    }
+
+
+}
+}
