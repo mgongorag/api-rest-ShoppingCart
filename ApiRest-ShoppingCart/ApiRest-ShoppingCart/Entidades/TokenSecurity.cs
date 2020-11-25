442999bc97885be06803badcdf5cdcf8fec3e8fd
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2020-11-24 20:30:11 -0600

    Se agregó la clase Token Security

---
+++ ApiRest-ShoppingCart/ApiRest-ShoppingCart/Entidades/TokenSecurity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiRest_ShoppingCart.Entidades
+{
+    public class TokenSecurity
+    {
+        public  int idToken { get; set; }
+        public  string toekn { get; set; }
+        public  int expiration { get; set; }
+        public  int idUsuario { get; set; }
+        public string fechaIngreso { get; set; }
+        public int estado { get; set; }
+        
+    }
+}
