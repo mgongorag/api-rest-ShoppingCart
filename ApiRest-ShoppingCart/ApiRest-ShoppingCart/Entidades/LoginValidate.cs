be79bed016ce2463f648d46cd58dda192fa50b57
MIGUEL ANGEL GONGORA GARCIA <mgongorag@miumg.edu.gt>
2020-11-25 22:21:08 -0600

    Se agrego la clase LoginValidate

---
+++ ApiRest-ShoppingCart/ApiRest-ShoppingCart/Entidades/LoginValidate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiRest_ShoppingCart.Entidades
+{
+    public class LoginValidate 
+    {
+        public  string email { get; set; }
+        public  string password { get; set; }
+
+    }
+}
